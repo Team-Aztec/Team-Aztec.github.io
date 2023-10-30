475b94b52331f23ffe5a71bb92ee0192ba84e431
aztec <teamaztec.com@gmail.com>
2023-10-30 11:38:13 +0100

    update website

--- app/aztec-app.js
+++ app/aztec-app.js
@@ -1,7 +1,7 @@
 const RENCONTRES = [
 	//[ "AAAA-MM-JJ hh:mm", "Text affiché", "https://www.twitch.tv/ironpol33" ]
-	[ "2023-10-23 20:30", "vs UNKNOWNS", "https://www.twitch.tv/ironpol33" ],
-	[ "2023-10-25 21:30", "vs Surfers", "https://www.twitch.tv/ironpol33" ],
+	[ "2023-10-30 20:30", "vs SnussyEsports", "https://www.twitch.tv/ironpol33" ],
+	[ "2023-11-01 20:30", "vs teamUSA", "https://www.twitch.tv/ironpol33" ],
 	[ "2023-09-20 20:30", "vs Into the Beach", "https://www.twitch.tv/ironpol33" ],
 
 ];
