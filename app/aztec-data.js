88bd5369a306935630e606bbe711f1fef77230df
aztec <teamaztec.com@gmail.com>
2024-01-11 19:45:14 +0100

    update website

--- app/aztec-data.js
+++ app/aztec-data.js
@@ -1,7 +1,7 @@
 export const RENCONTRES = [
 	//[ "AAAA-MM-JJ hh:mm", "Team/Talent", "Text affiché", "https://www.twitch.tv/teamaztec_tv" ],
 	[ "2024-01-05 21:00", "Team", "L'Echauffement BEHOP", "https://www.twitch.tv/djiimy" ],
-	[ "2024-01-05 20:30", "Talent", "ESEA Saison 48 Main", "https://www.twitch.tv/teamaztec_tv" ],
+	[ "2024-01-15 20:30", "Talent", "ESEA Saison 48 Main", "https://www.twitch.tv/teamaztec_tv" ],
 	[ "2024-01-15 20:30", "Team", "ESEA Saison 48 Intermediate", "https://www.twitch.tv/teamaztec_tv" ],
 ]
 
