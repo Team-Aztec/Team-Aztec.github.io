a60632faf6d560e8058270d22847eb95985f0905
aztec <teamaztec.com@gmail.com>
2024-01-16 02:07:54 +0100

    update website

--- app/aztec-data.js
+++ app/aztec-data.js
@@ -1,8 +1,8 @@
 export const RENCONTRES = [
 	//[ "AAAA-MM-JJ hh:mm", "Team/Talent", "Text affiché", "https://www.twitch.tv/teamaztec_tv" ],
 	[ "2024-01-05 21:00", "Team", "L'Echauffement BEHOP", "https://www.twitch.tv/djiimy" ],
-	[ "2024-01-16 20:00", "Talent", "vs mYinsanity", "https://www.twitch.tv/djiimy" ],
-	[ "2024-01-17 20:30", "Team", "vs ZAKAT", "https://www.twitch.tv/djiimy" ],
+	[ "2024-01-16 20:00", "Talent", "Aztec Talent vs mYinsanity", "https://www.twitch.tv/djiimy" ],
+	[ "2024-01-17 20:30", "Team", "Team Aztec vs ZAKAT", "https://www.twitch.tv/djiimy" ],
 ]
 
 
