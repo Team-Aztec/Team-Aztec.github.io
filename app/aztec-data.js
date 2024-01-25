3269859cc79a3706383afbcf2e48f97e26f24f2b
aztec <teamaztec.com@gmail.com>
2024-01-25 13:26:58 +0100

    update website

--- app/aztec-data.js
+++ app/aztec-data.js
@@ -6,7 +6,7 @@ export const RENCONTRES = [
 	[ "2024-01-24 21:00", "Team Aztec vs LAssos", "https://www.twitch.tv/djiimy", "13-11", "ESEA Intermediate", "https://www.faceit.com/fr/cs2/room/1-082b894d-9d07-47b3-b0af-9d21325fa241/scoreboard" ],
 	[ "2024-01-17 20:30", "Team Aztec vs ZAKAT", "https://www.twitch.tv/djiimy", "13-10", "ESEA Intermediate", "https://www.faceit.com/fr/cs2/room/1-f4b1bc09-ca20-465d-8705-499ad29a4c71/scoreboard" ],
 	[ "2024-01-25 21:00", "Team Aztec vs white Iverson", "https://www.twitch.tv/djiimy" ],
-	[ "2024-01-25 20:00", "Aztec Talent vs RGW Esports", "https://www.twitch.tv/djiimy" ],
+	[ "2024-02-02 20:00", "Aztec Talent vs RGW Esports", "https://www.twitch.tv/djiimy" ],
 	[ "2024-01-23 20:00", "Aztec Talent vs SHIPACHI", "https://www.twitch.tv/djiimy", "13-10", "ESEA Main", "https://www.faceit.com/fr/cs2/room/1-9222ca50-41b5-4de8-adde-8ee06fe6b028/scoreboard" ],	
 	[ "2024-01-30 20:00", "Aztec Talent vs GEEKZ Energy", "https://www.twitch.tv/djiimy" ],
 	[ "2024-02-01 20:00", "Aztec Talent vs Phantom troupe", "https://www.twitch.tv/djiimy" ],
