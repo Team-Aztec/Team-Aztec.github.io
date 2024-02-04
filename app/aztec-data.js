cf9fcb853fe263960aa9744e7d80f6996d2dec64
aztec <teamaztec.com@gmail.com>
2024-02-04 13:47:57 +0100

    update website

--- app/aztec-data.js
+++ app/aztec-data.js
@@ -13,7 +13,7 @@ export const RENCONTRES = [
 	[ "2024-01-30 20:00", "Team Aztec vs k0ncept kuwlaka", "https://www.twitch.tv/djiimy", "1-0", "ESEA Intermediate", "https://www.faceit.com/fr/cs2/room/1-adc3846e-c604-4a07-aaba-0a4b0ea292e5/scoreboard" ],
 	[ "2024-02-01 21:00", "Team Aztec vs Quickplay Grey", "https://www.twitch.tv/djiimy", "13-8", "ESEA Intermediate", "https://www.faceit.com/fr/cs2/room/1-363595cf-1c9a-4d47-8e43-9136ef090cd4/scoreboard" ],
 	[ "2024-02-06 20:00", "Team Aztec vs GTZ-Esports", "https://www.twitch.tv/djiimy" ],
-	[ "2024-02-08 20:00", "Team Aztec vs 95Vikings", "https://www.twitch.tv/djiimy" ],
+	[ "2024-02-12 21:00", "Team Aztec vs 95Vikings", "https://www.twitch.tv/djiimy" ],
 	[ "2024-02-06 20:00", "Aztec Talent vs 100", "https://www.twitch.tv/djiimy" ],
 	[ "2024-02-08 20:00", "Aztec Talent vs Griefers", "https://www.twitch.tv/djiimy" ],
 ]//NOT HERE
