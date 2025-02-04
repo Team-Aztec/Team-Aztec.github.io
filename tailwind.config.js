c05d22c3a481cc6109478c3d9b46bb73f5f40d30
Lucas VANTOMME <lvantomme@norauto.com>
2025-02-04 14:31:16 +0100

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      "mob-s": "320px",
+      "mob-m": "425px",
+      "mob-l": "576px",
+      "tab-s": "640px",
+      "tab-m": "768px",
+      "tab-l": "992px",
+      "lap-s": "1024px",
+      "lap-m": "1200px",
+      "lap-l": "1366px",
+      "lap-xl": "1440px",
+      "lap-2xl": "1680px",
+      "lap-3xl": "1920px",
+    },
+    colors: {
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
