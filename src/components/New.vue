65389bb6ca6e073c69efe8e1ce6805a456b19cef
Lucas VANTOMME <lvantomme@norauto.com>
2025-02-22 20:26:08 +0100

    feat: News

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="new">
-    <p class="new-title">News 1</p>
-    <p class="new-description"
-      >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquam venenatis volutpat. Nunc lacinia auctor tellus
-      eget scelerisque. Sed pulvinar lobortis aliquet. Sed cursus tellus eget nibh placerat dapibus. Ut sit amet rutrum erat, nec
-      dignissim turpis. Aenean laoreet velit in lacus dignissim mattis. Praesent posuere at lacus ut volutpat. Integer in tortor
-      volutpat, faucibus risus nec, euismod sapien. Vestibulum rhoncus, sapien quis blandit lobortis, neque arcu tempus tortor, in
-      sagittis justo nulla molestie ex. Pellentesque sollicitudin ante vitae neque faucibus vulputate. Aliquam erat volutpat.
-      Suspendisse luctus auctor dui. Maecenas sollicitudin tempus nunc vitae eleifend.</p
-    >
+    <div class="new-image">
+      <img src="./../assets/images/banniere.jpeg" alt="card-image" />
+    </div>
+    <div class="new-description">
+      <h6 class="new-description-title">Article</h6>
+      <p class="new-description-text">
+        The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to &quot;Naviglio&quot; where you can
+        enjoy the main night life in Barcelona.
+      </p>
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
   .new {
-    color: black;
-    background-color: white;
+    @apply relative flex flex-col my-6 bg-white shadow-sm border rounded-lg transition-all cursor-pointer w-96;
+
+    &:hover {
+      -webkit-box-shadow: 0px 5px 20px 3px rgba(255, 255, 255, 0.49);
+      box-shadow: 0px 5px 20px 3px rgba(255, 255, 255, 0.49);
+    }
+
+    &-image {
+      @apply relative h-56 m-2.5 overflow-hidden text-white rounded-md;
+    }
+
+    &-description {
+      @apply text-black p-4;
+
+      &-title {
+        @apply mb-2 text-xl font-semibold;
+      }
+
+      &-text {
+        @apply leading-normal font-light;
+      }
+    }
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ export default {
       'lap-3xl': '1920px',
     },
     colors: {
+      black: '#000000',
       white: '#FFFFFF',
       'main-color': '#f6a429',
     },
