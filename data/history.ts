d78bcaa53a78dff1a08218815fbf3e334f745961
Lucas VANTOMME <lucas.vantomme@hotmail.fr>
2025-05-13 14:46:29 +0200

    fix: fill history

--- data/history.ts
+++ data/history.ts
@@ -2,10 +2,42 @@ import { History } from '../src/types/types'
 
 export const history: History[] = [
   {
+    title: 'IconX - First Blood : Qualifier 1',
+    url: 'https://www.faceit.com/fr/championship/6372cec9-75b9-45e0-8a20-c9ee648b7e86/IconX%20-%20First%20Blood%20:%20Qualifier%201',
+    image: {
+      url: '/src/assets/history/iconX_first-blood.jpg',
+      alt: 'IconX - First Blood : Qualifier 1',
+    },
+  },
+  {
+    title: 'Last Chance - IESF QUALIFIER FRANCE',
+    url: 'https://www.faceit.com/fr/championship/603132ae-3ced-40ee-9a38-f987c9eb0f05/Last%20Chance%20-%20IESF%20QUALIFIER%20FRANCE',
+    image: {
+      url: '/src/assets/history/IESF_EEC25.jpeg',
+      alt: 'Last Chance - IESF QUALIFIER FRANCE',
+    },
+  },
+  {
+    title: 'IESF - QUALIFIER FRANCE 2',
+    url: 'https://www.faceit.com/fr/championship/e5a7f1d4-2e16-47aa-b92a-7d772dbc6124/IESF%20-%20QUALIFIER%20FRANCE%202',
+    image: {
+      url: '/src/assets/news/IESF_EEC25.jpeg',
+      alt: 'IESF - QUALIFIER FRANCE 2',
+    },
+  },
+  {
+    title: 'IESF - QUALIFIER FRANCE 1',
+    url: 'https://www.faceit.com/fr/championship/eff0ceb2-ffd5-4634-8bcc-2fc861156792/IESF%20-%20QUALIFIER%20FRANCE%201',
+    image: {
+      url: '/src/assets/news/IESF_EEC25.jpeg',
+      alt: 'IESF - QUALIFIER FRANCE 1',
+    },
+  },
+  {
     title: 'Aztec Arena 2',
     url: 'https://liquipedia.net/counterstrike/Aztec_Arena/2',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/arena.jpeg',
       alt: 'Aztec Arena 2',
     },
   },
@@ -13,7 +45,7 @@ export const history: History[] = [
     title: 'Aztec Arena 1',
     url: 'https://liquipedia.net/counterstrike/Aztec_Arena/1',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/arena.jpeg',
       alt: 'Aztec Arena 1',
     },
   },
@@ -21,7 +53,7 @@ export const history: History[] = [
     title: 'Aztec Cup 12',
     url: 'https://www.faceit.com/fr/championship/5284bce2-b0d0-4b9c-b66d-39ffc91881a3/Aztec%20CUP%2012',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 12',
     },
   },
@@ -29,7 +61,7 @@ export const history: History[] = [
     title: 'Aztec Cup 11',
     url: 'https://www.faceit.com/fr/championship/702a34e6-3849-4920-ac13-1d465e99d224/Aztec%20CUP%2011',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 11',
     },
   },
@@ -37,7 +69,7 @@ export const history: History[] = [
     title: 'Aztec Cup 10',
     url: 'https://www.faceit.com/fr/championship/75b23925-e434-47e2-a3b2-f1ed42c4943f/Aztec%20CUP%2010',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 10',
     },
   },
@@ -45,7 +77,7 @@ export const history: History[] = [
     title: 'Aztec Cup 9',
     url: 'https://www.faceit.com/fr/championship/6ce79479-4459-45f8-9eb0-0c1caaffdb0f/Aztec%20CUP%209',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 9',
     },
   },
@@ -53,7 +85,7 @@ export const history: History[] = [
     title: 'Aztec Cup 8',
     url: 'https://www.faceit.com/fr/championship/7e669f46-bd58-423a-9c3a-f7eba1c39182/Aztec%20CUP%208',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 8',
     },
   },
@@ -61,7 +93,7 @@ export const history: History[] = [
     title: 'Aztec Cup 7',
     url: 'https://www.faceit.com/fr/championship/c7900c67-2d05-43a4-a371-442d03433834/Aztec%20CUP%207',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 7',
     },
   },
@@ -69,7 +101,7 @@ export const history: History[] = [
     title: 'Aztec Cup 6',
     url: 'https://www.faceit.com/fr/championship/2c188b78-4b6c-4c01-ab28-8edca1e747a2/Aztec%20CUP%206',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 6',
     },
   },
@@ -77,7 +109,7 @@ export const history: History[] = [
     title: 'Aztec Cup 5',
     url: 'https://www.faceit.com/fr/championship/d7e227bf-04f9-41ab-844e-a05be529de98/Aztec%20CUP%205',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 5',
     },
   },
@@ -85,7 +117,7 @@ export const history: History[] = [
     title: 'Aztec Cup 4',
     url: 'https://www.faceit.com/fr/championship/d785be39-df8f-451c-838b-d12fbf8be754/Aztec%20CUP%204',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 4',
     },
   },
@@ -93,7 +125,7 @@ export const history: History[] = [
     title: 'Aztec Cup 3',
     url: 'https://www.faceit.com/fr/championship/d979b4d4-b06b-4ace-82e5-b8aafcae9a4f/Aztec%20CUP%203',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 3',
     },
   },
@@ -101,7 +133,7 @@ export const history: History[] = [
     title: 'Aztec Cup 2',
     url: 'https://www.faceit.com/fr/championship/259bef4d-8aa0-45c4-aba5-a0581e68ac0a/Aztec%20CUP%202',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 2',
     },
   },
@@ -109,7 +141,7 @@ export const history: History[] = [
     title: 'Aztec Cup 1',
     url: 'https://www.faceit.com/fr/championship/86b3bbf4-f162-4ef7-8f8e-1ec26253a232/Aztec%20CUP%201',
     image: {
-      url: '/src/assets/news/IESF_EEC25.jpeg',
+      url: '/src/assets/history/cup.webp',
       alt: 'Aztec Cup 1',
     },
   },
